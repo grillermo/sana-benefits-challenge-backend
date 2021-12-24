c680ae6e2931212b09ba95363f94c185c168666f
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2021-12-23 20:26:58 -0600

    Add JWT deny list
    A good practice is to have a way to deny some tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_24_013531) do
+ActiveRecord::Schema.define(version: 2021_12_24_022415) do
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti"
+    t.datetime "exp", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
