afa1f1e84226542dee4a32ee10282e3388d3c7e4
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2021-12-23 20:38:41 -0600

    Install FactoryBot and test helper gems
    A more dynamic way to generate fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -71,4 +71,16 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   
   config.expose_dsl_globally = true
+
+  config.include FactoryBot::Syntax::Methods
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
 end
